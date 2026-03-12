1de7ee608874feddfd3473ea9c4b9595b99f620f
Thomas Lazarus <lazarustda@gmail.com>
2026-03-11 21:42:28 -0500

    Adds lazygit to snacks config

--- lua/kickstart/plugins/snacks.lua
+++ lua/kickstart/plugins/snacks.lua
@@ -1,12 +1,39 @@
 return {
   'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
   ---@type snacks.Config
   opts = {
     dashboard = {
+      enabled = true,
       sections = {
         { icon = ' ', title = 'Recent Files', section = 'recent_files', cwd = true },
         { section = 'startup' },
       },
+    },
+    lazygit = { enabled = true },
+  },
+  keys = {
+    {
+      '<leader>gg',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'Lazygit',
+    },
+    {
+      '<leader>gf',
+      function()
+        Snacks.lazygit.log_file()
+      end,
+      desc = 'Lazygit Current File History',
+    },
+    {
+      '<leader>gl',
+      function()
+        Snacks.lazygit.log()
+      end,
+      desc = 'Lazygit Log (cwd)',
     },
   },
 }
